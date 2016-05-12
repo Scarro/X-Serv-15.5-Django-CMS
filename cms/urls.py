83c1f005d625f7789c2ba5f51377bd0d589b0213
scarro <sergioca22@gmail.com>
2016-05-12 19:55:37 +0200

    actualizando

--- cms/urls.py
+++ cms/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'cms'
 urlpatterns = [
-	url(r'^$', views.index, name="index"),
-	url(r'(?P<nombre>.*)/(?P<recurso>.*)$', views.agregar, name="agregar"),
+    url(r'^$', views.index, name="index"),
+    url(r'(?P<nombre>.*)/(?P<recurso>.*)$', views.agregar, name="agregar"),
 ]
--- cms/views.py
+++ cms/views.py
@@ -9,19 +9,18 @@ from .models import Pages
 
 @csrf_exempt
 def index(request):
-	template = "cms/basesite.html"
-	context = {
-		'pages': Pages.objects.all(),
-	}
-
-	return render(request, template, context)
+    template = "cms/basesite.html"
+    context = {
+        'pages':Pages.objects.all()
+    }
+    return render(request, template, context)
 
 def agregar(request, nombre, recurso):
-	try:
-		Pages.objects.get(nombre=nombre)
-	except (KeyError, Pages.DoesNotExist):
-		Pages.objects.create(nombre=nombre, recurso=recurso)
-		context = {
-			'pages': Pages.objects.all(),
-		}
-	return render(request, 'cms/basesite.html', context)
+    try:
+        Pages.objects.get(nombre=nombre)
+    except (KeyError, Pages.DoesNotExist):
+        Pages.objects.create(nombre=nombre, recurso=recurso)
+    context = {
+        'pages':Pages.objects.all()
+    }
+    return render(request, 'cms/basesite.html', context)
