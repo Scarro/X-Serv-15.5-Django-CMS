24b26f487c72710f745f11cba0400d8e6b044601
scarro <sergioca22@gmail.com>
2016-05-12 19:35:52 +0200

    actualizando

--- cms/models.py
+++ cms/models.py
@@ -1,7 +1,11 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
 # Create your models here.
-
 class Pages(models.Model):
-    name = models.CharField(max_length=32)
-    page = models.TextField()
+	nombre = models.CharField(max_length=200)
+	recurso = models.TextField()
+
+	def __str__(self):
+		return self.recurso
--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Pages
 
 # Create your views here.
+
+@csrf_exempt
+def index (request):
+	template = "cms/basesite.html"
+	context = {
+		'pages': Pages.objects.all(),
+	}
+
+	return render(request, template, context)
+
+def agregar(request, nombre, recurso):
+	try:
+		Pages.objects.get(nombre=nombre)
+	except (KeyError, Pages.DoesNotExist):
+		Pages.objects.create(nombre=nombre, recurso=recurso)
+
+	context = {
+		'pages': Pages.objects.all(),
+	}
+	return render(request, 'cms/basesite.html', context)
